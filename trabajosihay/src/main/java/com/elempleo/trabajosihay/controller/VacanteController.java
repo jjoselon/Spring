2191b95808a8662dbb899ecabfd179428346f1c9
José Alejandro Ramirez Rivera <jjoselon3167@hotmail.com>
2022-01-26 17:27:20 -0500

    music api proyect added

--- trabajosihay/src/main/java/com/elempleo/trabajosihay/controller/VacanteController.java
+++ trabajosihay/src/main/java/com/elempleo/trabajosihay/controller/VacanteController.java
@@ -6,8 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.elempleo.trabajosihay.model.Vacante;
 import com.elempleo.trabajosihay.repository.VacanteRepository;
@@ -40,5 +42,14 @@ public class VacanteController {
 		
 		model.addAttribute("vacante", vacante);
 		return "vacante/listvacantes";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteVacante(@RequestParam("id") int id, Model model) {
+		vacanteRepository.deleteById(id);
+		
+		model.addAttribute("id", id);
+		model.addAttribute("cantidad_registros", vacanteRepository.count());
+		return "vacante/deleted";
 	}
 }
